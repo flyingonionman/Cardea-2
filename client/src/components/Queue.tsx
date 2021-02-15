3616767bc9dcd62fd6ff402a377a005d8018debc
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2021-02-15 16:22:51 -0500

    rightside design done
    1. add transitions?
    2. add filters?

--- client/src/components/Queue.tsx
+++ client/src/components/Queue.tsx
@@ -1,22 +1,51 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Queueitem from "./Queueitem"
 import "../css/Queue.css"
+
+type Questitem = {
+    id: number;
+    name: string;
+}
+
+
 const Queue = () => {
-  
-    return(
+    const [itemarray, setitemarray] = useState<Questitem[]>([
+        { "id": 1, "name": "google", },
+        { "id": 2, "name": "googler", },
+        { "id": 3, "name": "googler", },
+        { "id": 4, "name": "googler", },
+        { "id": 5, "name": "googler", },
+        { "id": 6, "name": "googler", },
+
+    ])
+
+    useEffect(() => {
+        
+    }, [])
+
+    let variablelist = <ul id="queue_container">
+        <li className="joblist">
+            <h5>Company</h5>
+            <h5>Position</h5>
+            <h5 className="location">Location </h5>
+        </li>
+        {itemarray.map((e, i) => (
+            <Queueitem 
+                listname={e.name}
+                backgroundcolor={i}
+                key={i}
+            />
+        ))}
+    </ul>
+
+    return (
+
         <div id="queue">
             
             <h2>Queue</h2>
-
+            <hr></hr>
             <h4>tags : Cardea's favorites</h4>
-            <div id="queue_container">
-                <ul>
-                    <Queueitem listname={"boop"}/>
-                    <Queueitem listname={"boop"}/>
-                    <Queueitem listname={"boop"}/>
-
-                </ul>
-            </div>
+            {variablelist}
         </div>
     )
 }
--- client/src/components/Queueitem.tsx
+++ client/src/components/Queueitem.tsx
@@ -1,16 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 
 type Props = {
-    listname : string
+    listname: string
+    backgroundcolor: number
 }
 /* 
 Individual job playlists and their information    
 */
-const Queueitem = ({listname }: Props) => {
-  
+const Queueitem = ({ listname, backgroundcolor }: Props) => {
+    const [toggle, settoggle] = useState(false)
+
     return(
-        <li className="joblist">
-            {listname}
+        <li className={
+            "joblist " +
+            (backgroundcolor % 2 ? "" : "accent ") +
+            ((toggle) ? "description" : "")
+        }
+            onClick={()=>settoggle(!toggle)}
+        >
+            <h5>{listname}</h5>
+            <h5>Software engineer</h5>
+            <h5 className="location">LA/NY </h5>
         </li>
     )
 }
